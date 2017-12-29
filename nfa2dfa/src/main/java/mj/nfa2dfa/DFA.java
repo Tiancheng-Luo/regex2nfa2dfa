2182ae8a5511bc717595d67d55ffa3c118716036
mj zhang <mingjunzhang.cn@gmail.com>
2017-12-28 16:02:19 -0800

    bakup the code

---
+++ nfa2dfa/src/main/java/mj/nfa2dfa/DFA.java
@@ -0,0 +1,21 @@
+package mj.nfa2dfa;
+
+import java.util.LinkedList;
+
+public class DFA {
+	private LinkedList<State> dfa;
+	
+	public DFA () {
+		this.setDfa(new LinkedList<State> ());
+		this.getDfa().clear();
+	}
+
+	public LinkedList<State> getDfa() {
+		return dfa;
+	}
+
+	public void setDfa(LinkedList<State> nfa) {
+		this.dfa = nfa;
+	}
+}
+// This line make it work
---
+++ nfa2dfa/src/main/java/mj/nfa2dfa/NFA.java
@@ -0,0 +1,20 @@
+package mj.nfa2dfa;
+import java.util.LinkedList;
+
+public class NFA {
+	private LinkedList<State> nfa;
+	
+	public NFA () {
+		this.setNfa(new LinkedList<State> ());
+		this.getNfa().clear();
+	}
+
+	public LinkedList<State> getNfa() {
+		return nfa;
+	}
+
+	public void setNfa(LinkedList<State> nfa) {
+		this.nfa = nfa;
+	}
+}
+// This line make it work
